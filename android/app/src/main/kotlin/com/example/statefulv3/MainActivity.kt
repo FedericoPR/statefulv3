371eff2310369eaacafffd542b78f95d03a5ded3
FedericoPR <federico.pernas@alumnos.uva.es>
2020-10-15 14:04:23 +0200

    Formulario-password-iconos

---
+++ android/app/src/main/kotlin/com/example/statefulv3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.statefulv3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/formulario.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+/*
+class Formulario extends StatefulWidget{
+  @override
+  FormularioState createState() {
+    print(" Create de Formulario");
+    return new FormularioState();
+  }
+}
+
+class FormularioState extends State<Formulario>  {
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(" Hola");
+  }
+}
+*/
+
+class Formulario extends StatefulWidget{
+  @override
+  FormularioState createState() {
+    print(" Create de Formulario");
+    return new FormularioState();
+  }
+}
+
+class FormularioState extends State<Formulario>  {
+
+  bool _mostrar=false;
+  doclick(){
+    print("click");
+    setState(() {
+      _mostrar = !_mostrar;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return
+      Row(
+        children: [
+          Expanded(child:
+
+          TextField(
+            obscureText: !_mostrar,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Password',
+            ),
+          )
+          ),
+          //--------------------------
+          if(_mostrar)
+            IconButton(
+                icon: Icon(Icons.visibility),
+                onPressed: (){
+                  doclick();
+                })
+          else
+            IconButton(
+                icon: Icon(Icons.visibility_off),
+                onPressed: (){
+                  doclick();
+                })
+        ],
+      )
+        ;
+  }
+}
